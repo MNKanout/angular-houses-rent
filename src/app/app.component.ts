b30297b088f2807ec24bb99034f7cd9ff5a9318b
Nour Kanout <nkanout@identecsolutions.com>
2023-09-05 14:30:35 +0200

    Added the home component into the root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,16 @@ import { HomeComponent } from './home/home.component';
   imports: [
     HomeComponent,
   ],
-  template: `<h1>Hello World!</h1>`,
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
