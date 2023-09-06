8941ddc9fd7a095b0700fb60bbaa15581bc6a323
Nour <mnkanout@gmail.com>
2023-09-06 17:18:35 +0200

    Added a missing closing anchor tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,12 @@ import { RouterModule } from '@angular/router';
   ],
   template: `
   <main>
-    < a [routerLink]="['/']">
+    <a [routerLink]="['/']">
     <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" 
+      aria-hidden="true">
     </header>
+    </a>
     <section class="content">
       <router-outlet></router-outlet>
     </section>
